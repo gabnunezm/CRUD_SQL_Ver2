7c27b552f35247fa33878d7997d5a99ac90bd81f
Gabriel Nuñez <20231871@itla.edu.do>
2025-03-13 23:11:12 -0400

    Se arregló la conexión de la DB.

--- CapaDatos/CD_Conexion.cs
+++ CapaDatos/CD_Conexion.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +10,7 @@ namespace CapaDatos
 {
     public class CD_Conexion
     {
-        private SqlConnection Conexion = new SqlConnection("Server=(local);DataBase= Practica;Integrated Security=true");
+        private SqlConnection Conexion = new SqlConnection("Server=DESKTOP-QQ4E961;Database=Practica;Integrated Security=True;TrustServerCertificate=True");
 
         public SqlConnection AbrirConexion()
         {
